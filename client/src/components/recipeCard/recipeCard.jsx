999886e405e9a69d271c31ba1b7483c3c8feb670
gzucca <gonzalozucca_4@outlook.com>
2023-02-15 10:27:48 -0300

    Cambios en responsive RecipeCard

--- client/src/components/recipeCard/recipeCard.jsx
+++ client/src/components/recipeCard/recipeCard.jsx
@@ -1,55 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './recipeCard.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./recipeCard.css";
 
 function RecipeCard({ name, img, diets, id, dishTypes }) {
+	let dietsNewArray = (diets) => {
+		let newArray = [];
+		for (let i = 0; i < diets.length; i++) {
+			let diet = diets[i];
+			let dietUpper = diet.name.charAt(0).toUpperCase() + diet.name.slice(1);
+			newArray.push(dietUpper);
+			if (i + 1 < diets.length) {
+				newArray.push(", ");
+			}
+		}
+		newArray.push(".");
+		return newArray;
+	};
 
-    let dietsNewArray = (diets) => {
-        let newArray = []
-        for (let i = 0; i < diets.length; i++) {
-            let diet = diets[i];
-            let dietUpper = diet.name.charAt(0).toUpperCase() + diet.name.slice(1)
-            newArray.push(dietUpper)
-            if (i+1 < diets.length){
-            newArray.push(', ')}
-            }   
-            newArray.push('.')
-        return newArray
-    }
+	let dishTypesNewArray = (dishTypes) => {
+		let newArray = [];
+		for (let i = 0; i < dishTypes.length; i++) {
+			let dishType = dishTypes[i];
+			if (i === 0) {
+				let dishTypeUpper =
+					dishType.charAt(0).toUpperCase() + dishType.slice(1);
+				newArray.push(dishTypeUpper);
+			}
+			if (i !== 0) {
+				newArray.push(dishType);
+			}
+			if (i + 1 < dishTypes.length) {
+				newArray.push(", ");
+			}
+		}
+		newArray.push(".");
+		return newArray;
+	};
 
-    let dishTypesNewArray = (dishTypes) => {
-        let newArray = []
-        for (let i = 0; i < dishTypes.length; i++) {
-            let dishType = dishTypes[i];
-            if (i === 0){
-                let dishTypeUpper = dishType.charAt(0).toUpperCase() + dishType.slice(1)
-                newArray.push(dishTypeUpper)
-            } if (i !== 0){
-            newArray.push(dishType)
-            }
-            if (i+1 < dishTypes.length){
-            newArray.push(', ')}
-            }   
-            newArray.push('.')
-        return newArray
-    }
+	return (
+		<div className="recipeCard">
+			<Link className="header" to={`/recipe/${id}`}>
+				<h2>{name}</h2>
+			</Link>
+			<h4>
+				{dietsNewArray(diets).map((e) => {
+					return e;
+				})}
+			</h4>
 
+			{dishTypes.length && (
+				<h5>
+					{" "}
+					{dishTypesNewArray(dishTypes).map((e) => {
+						return e;
+					})}{" "}
+				</h5>
+			)}
 
-    return (
-        <div className='recipeCard' >
-            <Link className='header' to={`/recipe/${id}`}><h2>{name}</h2></Link>
-            <h4>
-                {dietsNewArray(diets).map(e => {
-                return e})}
-            </h4>
-            
-                {dishTypes.length && 
-                (<h5> {dishTypesNewArray(dishTypes).map(e => {return e})} </h5>)}
-            
-                <Link className='ImageContainer' to={`/recipe/${id}`}><img height="400px" src={img} alt="imagen" /></Link>
-        </div>
-    )
+			<Link  to={`/recipe/${id}`}>
+            <div className="ImageContainer">
+				<img src={img} alt="imagen" />
+            </div>
+			</Link>
+		</div>
+	);
 }
 
-export default RecipeCard
+export default RecipeCard;
