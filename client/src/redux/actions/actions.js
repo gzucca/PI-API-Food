e8e29e187d7a7e982d9c895bd726b2d7d4e36cb9
gonzalo.zucca@gmail.com <gonzalo.zucca@gmail.com>
2023-03-04 01:22:01 -0300

    Cambios front

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -10,85 +10,90 @@ export const GET_RECIPE_DETAIL = 'GET_RECIPE_DETAIL';
 
 
 
-export const getAllRecipes = () => { 
-    return async function (dispatch) {
-        try {
-            await fetch("http://localhost:3001/recipes")
-            .then(res => res.json())
-            .then(data => dispatch({ type: GET_ALL_RECIPES, payload: data }))
-        } catch (error) {
-            alert ('No se pudieron descargar recetas')
-            console.log(error)
-        }
+// const fetchApi = fetchAbsolute(fetch)('http://localhost:3001');
+const fetchApi = fetchAbsolute(fetch)('https://meaty-deer-production.up.railway.app')
+
+export const getAllRecipes = () => {
+  return async function(dispatch) {
+    try {
+      await fetchApi("/recipes")
+        .then(res => res.json())
+        .then(data => dispatch({ type: GET_ALL_RECIPES, payload: data }))
+
+    } catch (error) {
+      alert('No se pudieron descargar recetas')
+      console.log(error)
     }
+  }
 };
 
-export const loadAllRecipes = (payload) => { 
-    return { type: LOAD_ALL_RECIPES, payload: payload }
+export const loadAllRecipes = (payload) => {
+  return { type: LOAD_ALL_RECIPES, payload: payload }
 };
 
 
-export const getRecipes = (name) => { 
-    return async function (dispatch) {
-        try {
-            await fetch(`http://localhost:3001/recipes?name=${name}`)
-            .then(res => res.json())
-            .then(data => dispatch({ type: GET_RECIPES, payload: data }))
-        } catch (error) {
-            alert ('No se pudieron encontrar recetas para esa búsqueda')
-            console.log(error)
-        }
-        
+export const getRecipes = (name) => {
+  return async function(dispatch) {
+    try {
+      await fetchApi(`/recipes?name=${name}`)
+        .then(res => res.json())
+        .then(data => dispatch({ type: GET_RECIPES, payload: data }))
+    } catch (error) {
+      alert('No se pudieron encontrar recetas para esa búsqueda.')
+      console.log(error)
     }
+
+  }
 };
 
-export const getAllDiets = () => { 
-    return async function (dispatch) {
-            try {
-            await fetch("http://localhost:3001/diets")
-            .then(res => res.json())
-            .then(data => dispatch({ type: GET_ALL_DIETS, payload: data }))
-            } catch (error) {
-            alert ('No se pudieron descargar las dietas')
-            console.log(error)
-        }
+export const getAllDiets = () => {
+  return async function(dispatch) {
+    try {
+      await fetchApi("/diets")
+        .then(res => res.json())
+        .then(data => dispatch({ type: GET_ALL_DIETS, payload: data }))
+    } catch (error) {
+      alert('No se pudieron descargar las dietas')
+      console.log(error)
     }
+  }
 };
 
-export const getRecipeDetail = (id) => { 
-    return async function(dispatch) {
-        try {
-            await fetch(`http://localhost:3001/recipes/${id}`)
-            .then(res => res.json())
-            .then(data => dispatch({ type: GET_RECIPE_DETAIL, payload: data }))
-        } catch (error) {
-            alert('No se pudieron encontrar recetas con ese ID')
-            console.log(error)
-        }  
+export const getRecipeDetail = (id) => {
+  return async function(dispatch) {
+    try {
+      await fetchApi(`/recipes/${id}`)
+        .then(res => res.json())
+        .then(data => dispatch({ type: GET_RECIPE_DETAIL, payload: data }))
+    } catch (error) {
+      alert('No se pudieron encontrar recetas con ese ID')
+      console.log(error)
     }
+  }
 }
 
 
-export const filterRecipeDiets = (payload) => { 
-    return { type: FILTER_RECIPE_DIETS, payload: payload }
+export const filterRecipeDiets = (payload) => {
+  return { type: FILTER_RECIPE_DIETS, payload: payload }
 };
 
-export const cleanRecipeDetail = (payload) => { 
-    return { type: 'CLEAN_RECIPE', payload: payload }
+export const cleanRecipeDetail = (payload) => {
+  return { type: 'CLEAN_RECIPE', payload: payload }
 };
 
 
-export const sortRecipes = (payload) => { 
-    return { type: SORT_RECIPES, payload: payload }
+export const sortRecipes = (payload) => {
+  return { type: SORT_RECIPES, payload: payload }
 };
 
-export const createRecipe = (payload) => { 
-    return async function () { 
+export const createRecipe = (payload) => {
+  return async function() {
     try {
-        const res = await axios.post('http://localhost:3001/recipes', payload);
-        return res;
+      const res = await axios.post('recipes', payload);
+      return res;
     } catch (error) {
-        alert('No se pudo crear la receta')
-        console.log(error)}  
+      alert('No se pudo crear la receta')
+      console.log(error)
     }
+  }
 }
