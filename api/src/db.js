96fdeacd711b700adcceec7020e03bf2e5a40721
gonzalo.zucca@gmail.com <gonzalo.zucca@gmail.com>
2023-03-03 23:50:13 -0300

    Cambios url sequelize y CORS

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE
 } = process.env;
 
-const sequelize = new Sequelize(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`, {
+const sequelize = new Sequelize("postgresql://postgres:0V0Ch02B68BiMDbUEkBa@containers-us-west-97.railway.app:6012/railway", {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
