23c938aa822ae26f7de81412b744ca2f9bd2322f
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-05-10 17:10:37 +0200

    Changes to landing

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Background from "../../assets/clientBackground.webp";
+import BackgroundMobile from "../../assets/clientBackgroundMobile.webp";
 import "./Landing.css";
 
 export default function Landing() {
   return (
-    <main className="landingPage">
-      <Link to={`/home/`}>
-        <h1>HOME</h1>
-      </Link>
+    <main className="background">
+      <picture>
+        <source media="(max-width:800px)" srcset={BackgroundMobile} />
+        <img
+          className="background-image"
+          loading="eager"
+          src={Background}
+          alt="Background"
+        />
+      </picture>
+      <section className="landing">
+        <h1 className="landing-title">API FOOD</h1>
+        <h2 className="landing-subtitle">by Gonzalo Zucca</h2>
+        <Link to={`/home/`}>
+          <button className="landing-btn">HOME</button>
+        </Link>
+      </section>
     </main>
   );
-};
-
+}
